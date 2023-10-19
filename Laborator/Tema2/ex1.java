5094eebc702a5c906a9439a2d98b1e47affeddb2
Marius <110856060+sadwipe@users.noreply.github.com>
2023-10-19 14:29:25 +0300

    replaced the bool variable in function with return statements

--- Laborator/Tema2/ex1.java
+++ Laborator/Tema2/ex1.java
@@ -20,14 +20,12 @@ class ClasaParinte {
 	}
 	
 	static boolean isPrime(int n) {
-		boolean flag = true;
 		for (int i = 2; i < n; i++) {
 			if (n % i == 0) {
-				flag = false;
-				break;
+				return false;
 			}
 		}
 
-		return flag;
+		return true;
 	}
 }
